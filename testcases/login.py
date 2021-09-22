1a781fd63f48b776d4253fbf76378222f5977ae2
‘shouhuqingtian’ <519328539@qq.com>
2021-09-22 15:33:31 +0800

    登录

---
+++ testcases/login.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class AppDynamicsJob(unittest.TestCase):
+    def setUp(self):
+        # AppDynamics will automatically override this web driver
+        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        # self.base_url = "https://www.google.com/"
+        self.driver.get("https://gdnymall.com/login.html#/")
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_app_dynamics_job(self):
+        driver = self.driver
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("18575588381")
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("123456")
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div").click()
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        driver.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='您的密码长期未修改，为保障您的账号安全，建议尽快修改'])[1]/following::button[1]").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        # To know more about the difference between verify and assert,
+        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
